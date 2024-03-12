905ffa8ba2dffdead48dd3637452e9ba36cc6bb7
JabriilOsman <104216862+JabriilOsman@users.noreply.github.com.>
2024-03-12 14:10:25 +0100

    Fullført

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start,slutt) {
+    let total = 0;
 
+    for(let i = start; i <= slutt; i++ ){
+        total += i;
+    }
+
+
+    return total;
 };
 
 // Do not edit below this line
