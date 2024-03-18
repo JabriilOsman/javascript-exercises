1efccb81f899d26400a87c0659cb2fc607768bec
JabriilOsman <104216862+JabriilOsman@users.noreply.github.com.>
2024-03-18 19:38:36 +0100

    Fullført, ganske enkel oppgave.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(grader) {
-// fra farenheit til celsius
 let celsius = 0;
+
 celsius = (grader - 32) * 5/9;
 let totaltGrader = Math.round(celsius * 10) / 10
- return totaltGrader;
+return totaltGrader;
 
 
 
@@ -11,6 +11,7 @@ let totaltGrader = Math.round(celsius * 10) / 10
 
 const convertToFahrenheit = function(grader) {
   let fahrenheit = 0;
+  
   fahrenheit = (grader * 9/5) + 32;
   let totaltGrader = Math.round(fahrenheit * 10) / 10
   return totaltGrader;
