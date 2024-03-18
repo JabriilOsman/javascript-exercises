ea92e3e4c72a7346562541d99dc49e9f15a74b30
JabriilOsman <104216862+JabriilOsman@users.noreply.github.com.>
2024-03-18 19:15:50 +0100

    Fullført denne oppgaven

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(år) {
+    if((år % 4 === 0) && !(år % 100 ===0) || år % 400 ===0 ){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
 
 };
 
